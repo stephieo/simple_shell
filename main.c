158c5e18212f4523b2e5f0ac5da522b0c4e365b4
Stephanie Olulesho <43866127+stephieo@users.noreply.github.com>
2023-10-10 12:39:03 +0000

    main program

---
+++ main.c
@@ -0,0 +1,57 @@
+#include  "main.h"
+
+/**
+ * main- entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * @env: array of env variables
+ *
+ * Return: 1( success)
+ */
+
+int main(int __attribute__((unused)) argc,
+	 __attribute__((unused)) char **argv,
+	__attribute__((unused)) char **env
+	)
+{
+	pid_t pid = 0;
+	ssize_t gt_status;
+	char *cmdline = NULL, *newentry = NULL;
+	size_t n = 50;
+	const char *delim = " \t\f\r\n";
+	int ac = 0, cmdstatus = 0, exec_return, trigger;
+	char **av = NULL, **newenviron = NULL;
+
+	break_loop = 1;
+	while (break_loop)
+	{
+		print_prompt();
+		signal(SIGINT, my_sigint);
+		gt_status = line_reader(&cmdline, &n);
+		if (gt_status == 1)
+			continue;
+		else if (gt_status == -1)
+			exit(0);
+		av = token_generator(cmdline, delim, &ac);
+		if ((is_builtin_cmd(av, cmdline, cmdstatus, &newenviron,
+				 &newentry, ac)) == 0)
+			continue;
+		else if (access(av[0], F_OK) != 0)
+		{
+			exec_return = exec_from_path(env, pid, av, &cmdstatus);
+			trigger = exec_cleanup(exec_return, ac, av);
+			if (trigger == 127)
+				break;
+			else if (trigger == -1)
+				continue;
+		}
+		else if ((access(av[0], F_OK) == 0))
+		{
+			if ((exec_cmd(av, pid, &cmdstatus)) == -1)
+				continue;
+		}
+	clean_cmd(ac, av);
+	}
+	free(cmdline);
+	return (trigger == 127 ? 127 : 0);
+}
---
+++ main_functions_0.c
@@ -0,0 +1,105 @@
+#include "main.h"
+
+/**
+ * print_prompt - Print the shell prompt
+ */
+
+void print_prompt(void)
+{
+        if (isatty(STDIN_FILENO))
+                write(STDIN_FILENO, "$ ", 2);
+}
+
+/**
+ * line_reader - Reads the command line
+ * @buf: Pointer to hold the command line
+ * @n: size of the buffer
+ * Return: the number of characters read in the line,
+ * -1(getline error), 1( blank spaces)
+*/
+
+
+ssize_t line_reader(char **buf, size_t *n)
+{
+        ssize_t shellstate = getline(buf, n, stdin);
+
+        if (shellstate == -1)
+        {
+                if (isatty(STDIN_FILENO))
+                        write(STDOUT_FILENO, "\n", 1);
+                free(*buf);
+                return (-1);
+        }
+        if (_strspn(*buf, " \t\n\f\r") == _strlen(*buf))
+        {
+                return (1);
+        }
+
+        return (shellstate);
+}
+/**
+ * token_generator - Tokenize the cmdline into a list of strings
+ * @cmdline: the cmdline to tokenize
+ * @delim: Delimiter string
+ * @ac: pointer to hold the number of tokens
+ * Return: list of tokens
+ */
+
+char **token_generator(char *cmdline, const char *delim, int *ac)
+{
+	char *buf1 = _strdup(cmdline), *buf2 = _strdup(cmdline), *newtok, **av = NULL;
+	int i, toklen;
+
+	*ac = 0;
+	newtok = strtok(buf1, delim);
+	while (newtok)
+	{
+		newtok = strtok(NULL, delim);
+		(*ac)++;
+	}
+	av = malloc(sizeof(char *) * ((*ac) + 1));
+	if (!av)
+	{
+		perror("Memory allocation error");
+		free(buf2);
+		return (NULL);
+	}
+	for (i = 0; i < *ac; i++)
+		av[i] = NULL;
+	newtok = strtok(buf2, delim);
+	i = 0;
+	while (newtok)
+	{
+		toklen = _strlen(newtok);
+		av[i] = malloc(sizeof(char) * (toklen + 1));
+		if (av[i] == NULL)
+		{
+			perror("Memory allocation error");
+			for (i = 0; i < (*ac); i++)
+				free(av[i]);
+			exit(1);
+		}
+		_strcpy(av[i], newtok);
+		newtok = strtok(NULL, delim);
+		i++;
+	}
+	av[*ac] = NULL;
+	free(buf1);
+	free(buf2);
+	return (av);
+}
+
+/**
+ * clean_cmd - cleans up av and its elements
+ * @ac: argument count
+ * @av: argument vector
+ */
+
+void clean_cmd(int __attribute__((unused)) ac, char **av)
+{
+	int i;
+
+	for (i = 0; i < ac || av[i] != NULL; i++)
+		free(av[i]);
+	free(av);
+}
